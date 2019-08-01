8cca9443b15215c494e9b2eadb30366820a7207e
Sammy <sami.shamailov@gmail.com>
2019-08-01 21:34:28 +0300

    added error handler

--- src/middleware/error.ts
+++ src/middleware/error.ts
@@ -11,16 +11,3 @@ export function inputError(err: Error, req: Request, res: Response, next: NextFu
     }
     next(err);
 }
-
-// export function clientErrorHandler(err: Error, req: Request, res: Response, next: NextFunction) {
-//   if (!req.xhr) {
-//     res.status(500).send({ error: 'Something failed!' });
-//   } else {
-//     next(err);
-//   }
-// }
-
-// export function errorHandler(err: Error, req: Request, res: Response, next: NextFunction) {
-//   res.status(500);
-//   res.render('error', { error: err });
-// }

50cd7cda4682c5a0507e2d5f7ee941ad89ed6ad3
Sammy <sami.shamailov@gmail.com>
2019-08-01 20:46:57 +0300

    added input error handler use

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import { router as productsRouter } from './routes/products';
 import { router as categoriesRouter } from './routes/categories';
+import { inputError } from './middleware/error';
 
 
 const app = express();
@@ -12,6 +13,8 @@ app.use(cors());
 
 app.use('/products', productsRouter);
 app.use('/categories', categoriesRouter);
+
+app.use(inputError);
 
 
 export { app };

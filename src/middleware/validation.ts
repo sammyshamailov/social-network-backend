6e681938db2d629bfe53eda98527b378f5bc8d04
Sammy <sami.shamailov@gmail.com>
2019-08-01 22:42:16 +0300

    validation with joi

--- src/middleware/validation.ts
+++ src/middleware/validation.ts
@@ -1,20 +1,25 @@
 import { Request, Response, NextFunction } from 'express';
 import { Product } from '../models';
+import joi from 'joi';
+import {productNameSchema, idSchema} from '../validations/joiSchemas'
 
 export function nameValidation(req: Request, res: Response, next: NextFunction) {
     
     const product: Product = req.body;
-
-    if((!product.name) || (product.name.length < 3)){
-        next(new Error('name'));
-    }
+    const {error, value: v} = joi.validate(product, productNameSchema);
+    if (error) throw new Error('name');
+    // if((!product.name) || (product.name.length < 3)){
+    //     next(new Error('name'));
+    // }
     next();
 }
 
 export function idValidation(req: Request, res: Response, next: NextFunction) {
     
-    if (req.params.id.length < 36){
-        next(new Error('id'));
-    }
+    const {error, value: v} = joi.validate(req.params.id, idSchema);
+    if (error) throw new Error('id');
+    // if (req.params.id.length < 36){
+    //     next(new Error('id'));
+    // }
     next();
   }

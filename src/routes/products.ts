dbe12abb7d68932473ffdceeb67591bdaacdf48c
Sammy <sami.shamailov@gmail.com>
2019-08-01 19:20:25 +0300

    using validation middleware

--- src/routes/products.ts
+++ src/routes/products.ts
@@ -1,97 +1,86 @@
 import { Request, Response, NextFunction, Router } from 'express';
 import { ProductData, Product } from '../models';
 import uuidv1 from 'uuid/v1';
+import { nameValidation, idValidation } from '../middleware/validation';
 
 const products: Product[] = ProductData;
-  
-  
+
+
 function findProductIndex(req: Request, res: Response, next: NextFunction) {
   const id = req.params.id;
   const matchingIndex = products.findIndex(o => o.id === id);
-  
-  if (id.length < 36){
-    res.sendStatus(404);
-    return;
-  }
 
-  else if (matchingIndex < 0) {
+  if (matchingIndex < 0) {
     res.sendStatus(400);
     return;
   }
-  
+
   res.locals.matchingIndex = matchingIndex;
   res.locals.productId = id;
   next();
 }
-  
+
 const router = Router();
-  
+
 router.get('/', (req, res) => {
   res.send(products);
 });
 
-router.get('/:id', (req, res) => {
-  const id = req.params.id;
-  const matching = products.find(o => o.id === id);
-  
-  if (id.length < 36){
-    res.sendStatus(404);
-    return;
-  }
-
-  else if (!matching) {
-    res.sendStatus(400);
-    return;
-  }
-  
-  res.send(matching);
-});
-  
-router.post('/', (req, res) => {
-  const product: Product = req.body;
-
-  if((!product.name) || (product.name.length < 3)){
-    res.sendStatus(409);
-    return;
-  }
-  
-  product.id = uuidv1();
-
-  if(!product.categoryId){
-    product.categoryId = uuidv1();
-  }
-
-  products.push(product);
-  res.status(201).send(product);
-});
-  
-router.put('/:id',
-  findProductIndex,
+router.get('/:id',
+  idValidation,
   (req, res) => {
-    const { productId, matchingIndex } = res.locals;
-  
-    const product: Product = req.body;
+    const id = req.params.id;
+    const matching = products.find(o => o.id === id);
 
-    if((!product.name) || (product.name.length < 3)){
-      res.sendStatus(409);
+    if (!matching) {
+      res.sendStatus(400);
       return;
     }
 
+    res.send(matching);
+  });
+
+router.post('/',
+  nameValidation,
+  (req, res) => {
+
+    const product: Product = req.body;
+
+    product.id = uuidv1();
+
+    if (!product.categoryId) {
+      product.categoryId = uuidv1();
+    }
+
+    products.push(product);
+    res.status(201).send(product);
+  });
+
+router.put('/:id',
+  idValidation,
+  nameValidation,
+  findProductIndex,
+  (req, res) => {
+
+    const { productId, matchingIndex } = res.locals;
+    const product: Product = req.body;
+
     product.id = productId;
     products[matchingIndex] = product;
-  
+
     res.send(product);
   },
 );
-  
+
 router.delete('/:id',
+  idValidation,
   findProductIndex,
   (req, res) => {
+
     products.splice(res.locals.matchingIndex, 1);
-  
+
     res.sendStatus(204);
   },
 );
-  
+
 export { router };
-  

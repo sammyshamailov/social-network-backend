1560cedacf70fbccdd5835b774658b663f658e25
Sammy <sami.shamailov@gmail.com>
2019-08-02 11:29:56 +0300

    serving static files

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import cors from 'cors';
 import { router as productsRouter } from './routes/products';
 import { router as categoriesRouter } from './routes/categories';
 import { inputError } from './middleware/error';
+import path from 'path';
 
 
 const app = express();
@@ -10,6 +11,8 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
+
+app.use('/public', express.static(path.join(__dirname, 'static')));
 
 app.use('/products', productsRouter);
 app.use('/categories', categoriesRouter);

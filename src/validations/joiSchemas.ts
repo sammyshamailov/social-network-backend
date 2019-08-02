84ae7461866a7c2e0707de1d12a86148d8cdde46
Sammy <sami.shamailov@gmail.com>
2019-08-03 01:00:02 +0300

    changed joi object

--- src/validations/joiSchemas.ts
+++ src/validations/joiSchemas.ts
@@ -1,8 +1,6 @@
 import joi from 'joi';
 
-export const productNameSchema = joi.object().keys({
-  name: joi.string().min(5).required(),
-});
+export const productNameSchema = joi.string().min(3).required();
 
 export const idSchema = joi.string().min(36).max(36);
   

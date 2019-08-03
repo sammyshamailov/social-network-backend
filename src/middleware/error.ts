06565af6ff44abdf8f6ec366ebfbc822320d30b5
Sammy <sami.shamailov@gmail.com>
2019-08-03 15:35:10 +0300

    added error enum

--- src/middleware/error.ts
+++ src/middleware/error.ts
@@ -1,13 +1,15 @@
 import { Request, Response, NextFunction } from 'express';
 
 export function inputError(err: Error, req: Request, res: Response, next: NextFunction) {
-    if(err.message === 'name'){
+    if(err.message === errorType.name){
         res.status(400).send('product name is less than 3 chars or null');;
         return;
     }
-    else if(err.message === 'id'){
+    else if(err.message === errorType.id){
         res.status(400).send('id is less than 36 chars');
         return;
     }
     next(err);
 }
+
+enum errorType {name = 'name', id = "id"};
